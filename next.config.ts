9e6a7196198110a78844ab62e65018943a410cb1
Subhra <subhra.data@gmail.com>
2026-03-26 13:32:09 +0100

    Use root config for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,11 @@
 import type { NextConfig } from "next";
 
-const repoName = "drsubhra_new"; // 👈 your GitHub repo name
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-
-  // 👇 CRITICAL for GitHub Pages
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
-
-  // Performance & security
   poweredByHeader: false,
   productionBrowserSourceMaps: false,
   compress: true,
-
   images: {
     unoptimized: true,
   },
